69a05b26262fb1a46b9ac1f6d72b924db18379a6
Dollarkillerx <dollarkiller@vip.qq.com>
2019-09-27 11:41:45 +0800

    add

--- test/one_test.go
+++ test/one_test.go
@@ -7,8 +7,11 @@
 package test
 
 import (
+	"github.com/dollarkillerx/easyutils/clog"
+	"github.com/dollarkillerx/easyutils/httplib"
 	"log"
 	"strings"
+	"sync"
 	"testing"
 )
 
@@ -21,4 +24,28 @@ func TestOne(t *testing.T) {
 	split := strings.Split(str, ",")
 	log.Println(split)
 	log.Println(len(split))
+}
+
+// 压力测试
+func TestAb(t *testing.T) {
+	url := "https://translate.dollarkiller.com/translate?sl=&tl=fr&text=hello"
+	gcf(url,5000)
+}
+
+func gcf(str string,num int)  {
+	sy := sync.WaitGroup{}
+	for i:=0;i<num;i++ {
+		sy.Add(1)
+		go func() {
+			defer sy.Done()
+			bytes, e := httplib.EuUserGet(str)
+			if e != nil {
+				clog.PrintWa(e)
+			}else {
+				clog.Println(string(bytes))
+			}
+		}()
+	}
+
+	sy.Wait()
 }
